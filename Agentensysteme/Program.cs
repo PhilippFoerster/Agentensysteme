82bccd0907aa0bf15bf04dbf6cf8501c9c35d2da
Philipp <ph.foerster@outlook.de>
2021-11-15 17:06:49 +0100

    Projektdateien hinzufügen.

---
+++ Agentensysteme/Program.cs
@@ -0,0 +1,22 @@
+﻿using Agentensysteme;
+
+var (count1, matrix1) = await Utils.Import("Files/daten2A.txt");
+var (count2, matrix2) = await Utils.Import("Files/daten2B.txt");
+var (count3, matrix3) = await Utils.Import("Files/daten3A.txt");
+var (count4, matrix4) = await Utils.Import("Files/daten3B.txt");
+
+//var order1 = Utils.GetRandomOrder(Math.Min(count1, count2));
+var order2 = Utils.GetRandomOrder(Math.Min(count3, count4));
+var order1 = new List<int> { 0, 1, 2, 3 ,4 ,5 ,6 ,7 ,8 ,9 };
+
+var cost1 = Utils.CalculateCost(matrix1, order1);
+var cost2 = Utils.CalculateCost(matrix2, order1);
+var cost3 = Utils.CalculateCost(matrix3, order2);
+var cost4 = Utils.CalculateCost(matrix4, order2);
+
+Console.WriteLine($"Order: {string.Join(", ", order1)}");
+Console.WriteLine($"Cost1: {cost1}");
+Console.WriteLine($"Cost2: {cost2}");
+Console.WriteLine($"Order: {string.Join(", ", order2)}");
+Console.WriteLine($"Cost3: {cost3}");
+Console.WriteLine($"Cost4: {cost4}");
---
+++ Agentensysteme/Utils.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Agentensysteme
+{
+    public static class Utils
+    {
+        public static async Task<(int count, List<List<int>> matrix)> Import(string path)
+        {
+            var input = await File.ReadAllLinesAsync(path);
+            return (Convert.ToInt32(input[0]), input.Skip(1).Where(x => x.Trim() != "").Select(x => x.Trim().Split(" ").Select(xx => Convert.ToInt32(xx)).ToList()).ToList());
+        }
+
+        public static int CalculateCost(List<List<int>> matrix, List<int> order) 
+        {
+            int sum = 0;
+            for (int i = 1; i < order.Count; i++)
+            {
+                int previous = order[i - 1];
+                int current = order[i];
+                sum += matrix[previous][current];
+            }
+            return sum;
+        }
+
+        public static List<int> GetRandomOrder(int count)
+        {
+            Random random = new Random();
+            var numbers = Enumerable.Range(0, count);
+            return numbers.OrderBy(x => random.Next()).ToList();
+        }
+    }
+}
